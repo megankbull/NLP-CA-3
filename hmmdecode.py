3c39a13cbea8d0772a58e8cf3318115df01c4599
Megan Bull <85566817+megankbull@users.noreply.github.com>
2022-10-04 20:04:50 -0700

    attempt to classify unseen via suffixes

--- hmmdecode.py
+++ hmmdecode.py
@@ -14,51 +14,95 @@ N_UNIQ_TAG = None
 T_MAT = None
 E_MAT = None
 T_c = None
+LOWER_CORP = None 
+SET_CORP = None
+COMMON_TAGS = None
+SUFF_TAGS = None
 
 def read_file(f_path=sys.argv[1]):
    with open(f_path, 'r') as f: 
-      lines = f.readlines()
-
-   return [line.strip().split(" ") for line in lines]
+      return [l.strip().split(" ") for l in f.readlines()]
 
 def word_tuples(lines): 
+   
    tagged_tupes = []
+
    for line in lines: 
       line_tupes = []
       for token in line: 
          tok = token.strip()
          tag = tok.split("/")[-1]
+
          w = tok.replace(f"/{tag}", '')
          line_tupes.append((w, tag))
+
       tagged_tupes.append(line_tupes)
+
    return tagged_tupes
 
+def get_closest_index(word, last_state, prev_p): 
+   global CORPUS, T_c, E_MAT, T_MAT
+
+   try:
+      return CORPUS.index(word)
+
+   except ValueError: 
+      indices = [i for i, vocab_w in enumerate(CORPUS) if word.lower() == vocab_w.lower()]
+      
+      cur_p = np.Inf
+      
+      for loc in indices: 
+         s = []
+         for j in range(len(T_c)): 
+            e_p = E_MAT[loc, j]
+            t_p = T_MAT[last_state, j] 
+            s.append(e_p * t_p * prev_p)
+
+         state_p = max(s) 
+
+         if state_p > cur_p: 
+            cur_p = state_p 
+            best_tag = T_c[s.index(state_p)]
+            
+      return best_tag, cur_p
+
+def suffix_tag_p(word, tag):
+   global SUFF_TAGS
+
+   if len(word) < 3: return 1
+
+   if word[-3:] not in SUFF_TAGS.keys(): return 0
+   if tag not in SUFF_TAGS[word[-3:]].keys(): return 0
+
+   return SUFF_TAGS[word[-3:]][tag] / sum(SUFF_TAGS[word[-3:]].values())
+
 def viterbi_algo(word_seq):
-   global CORPUS, TAGS, N_UNIQ_TAG, E_MAT, T_MAT, T_c
-   
+   global TAGS, E_MAT, T_MAT, T_c, LOWER_CORP, SET_CORP, COMMON_TAGS
+
    states = []
    last_state = TAGS.index('')
-   common_tags_p = heapq.nlargest(6, N_UNIQ_TAG.items(), key=operator.itemgetter(1))
-   common_tags = {t for t,_ in common_tags_p}
+   prev_p = 1
 
    for i, w in enumerate(word_seq): 
       p = []
-      unseen = False if w.lower() in set(CORPUS) else True
-      c_i = CORPUS.index(w.lower()) if not unseen else 0 
+      unseen = w not in SET_CORP and w.lower() not in LOWER_CORP
+      c_i = 0 if unseen else get_closest_index(w, last_state, prev_p)
 
+      if type(c_i) is tuple: 
+         states.append(c_i[0])
+         last_state = TAGS.index(c_i[0])
+         prev_p = c_i[1]
+         continue 
 
       for j, tag in enumerate(T_c):
-         
+         suff_p = suffix_tag_p(w, tag) if unseen else 1
          e_p = 1 if unseen or i == 0 else E_MAT[c_i, j]
-         
-         if unseen and tag.lower() not in common_tags: t_p = 0
-         else: t_p = T_MAT[last_state,j]
+         t_p = 0 if unseen and tag not in COMMON_TAGS else T_MAT[last_state, j]
 
-         p.append(e_p * t_p)
+         p.append(e_p * t_p * prev_p * suff_p)
 
-      p_max = max(p)
-      best_tag_index = p.index(p_max)
-      best_state = T_c[best_tag_index]
+      prev_p = max(p)
+      best_state = T_c[p.index(prev_p)]
       states.append(best_state)
       last_state = TAGS.index(best_state)
 
@@ -73,8 +117,8 @@ def tag_input():
       for p_line in preds: 
          for i, p_tup in enumerate(p_line):
             if i == 0: 
-               f.write(f"{p_tup[0]}/{p_tup[1].upper()}")
-            else: f.write(f" {p_tup[0]}/{p_tup[1].upper()}")
+               f.write(f"{p_tup[0]}/{p_tup[1]}")
+            else: f.write(f" {p_tup[0]}/{p_tup[1]}")
          f.write("\n")
 
 def clean_matrix(matr_list):
@@ -89,22 +133,29 @@ def clean_matrix(matr_list):
    return np.asmatrix(matr)
 
 def set_model(): 
-   global CORPUS, TAGS, N_UNIQ_TAG, E_MAT, T_MAT, T_c
+   global CORPUS, TAGS, N_UNIQ_TAG, T_c, LOWER_CORP, SET_CORP, COMMON_TAGS, SUFF_TAGS, E_MAT, T_MAT
 
    with open(MODEL_PATH, "r") as f: 
       raw_params = f.read().split("\n\n")
       CORPUS = raw_params[1].strip().split(" ")
-      N_UNIQ_TAG = json.loads(raw_params[3])
-      TAGS = json.loads(raw_params[5])
+      SUFF_TAGS = json.loads(raw_params[3])
+      N_UNIQ_TAG = json.loads(raw_params[5])
+      TAGS = json.loads(raw_params[7])
 
-      e_mat = raw_params[7].strip().split(']')[:-2]
-      t_mat = raw_params[9].strip().split(']')[:-2]
+      e_mat = raw_params[9].strip().split(']')[:-2]
+      t_mat = raw_params[11].strip().split(']')[:-2]
    
    T_c = TAGS.copy()
    T_c.remove('')
 
    E_MAT = clean_matrix(e_mat)
    T_MAT = clean_matrix(t_mat)
+
+   LOWER_CORP = {w.lower() for w in CORPUS}
+   SET_CORP = set(CORPUS)
+
+   common_tags_p = heapq.nlargest(6, N_UNIQ_TAG.items(), key=operator.itemgetter(1))
+   COMMON_TAGS = {t for t,_ in common_tags_p}
 
 def main(): 
 
--- hmmlearn.py
+++ hmmlearn.py
@@ -5,118 +5,131 @@ import numpy as np
 
 OUT_PATH = "hmmmodel.txt"
 
+# find most likely tag to end sentence with 
+# sliding trigram? 
+# - choose tag with highest future state probability based on 
+
 CORPUS = None
 N_UNIQ_TAG = None
 TAGS = None
 T_MAT = None
 E_MAT = None
 T_c = None
+SUFF_TAG_COUNTS = None 
 
 def read_file(f_path=sys.argv[1]):
    with open(f_path, 'r') as f: 
-      lines = f.readlines()
+      return [l.strip().split(" ") for l in f.readlines()]
 
-   return [line.lower().strip().split(" ") for line in lines]
+def word_tuples(lines):
 
-def word_tuples(lines): 
    tagged_tupes = []
+   
    for line in lines: 
       line_tupes = []
       for token in line: 
          tok = token.strip()
          tag = tok.split("/")[-1]
+
          w = tok.replace(f"/{tag}", '')
          line_tupes.append((w, tag))
+
       tagged_tupes.append(line_tupes)
+      
    return tagged_tupes
 
-def get_tag_counts(line_tuples): 
+def get_tag_counts(): 
 
-   tag_counts = {'':  []}
+   line_tuples = word_tuples(read_file())
+
+   tag_counts = {'':[]}
    tag_bigram = {}
    word_tag_counts = {}
+   suff_tag_counts = {}
 
    for line in line_tuples: 
-      prev_tag = None
+      prev_tag = ''
       for word, tag in line: 
-         if prev_tag is None: tag_counts[''].append(word)
+         suff = word[-3:] if len(word) > 3 else ''
 
+         if prev_tag == '': tag_counts[''].append(word)
          if tag not in tag_counts.keys(): tag_counts[tag] = []
-         tag_counts[tag].append(word)
+
+         if suff not in suff_tag_counts.keys(): suff_tag_counts[suff] = {}
+         if tag not in suff_tag_counts[suff].keys(): suff_tag_counts[suff][tag] = 0
 
          if word not in word_tag_counts.keys(): word_tag_counts[word] = {}
          if tag not in word_tag_counts[word].keys(): word_tag_counts[word][tag] = 0
          
-         word_tag_counts[word][tag] +=1
-
-         bi_tags = (prev_tag, tag) if prev_tag is not None else ('', tag)
-         
+         bi_tags = (prev_tag, tag)
          if bi_tags not in tag_bigram.keys(): tag_bigram[bi_tags] = 0
          
-         tag_bigram[bi_tags]+=1
+         tag_counts[tag].append(word)
+         word_tag_counts[word][tag] += 1
+         suff_tag_counts[suff][tag] += 1
+         tag_bigram[bi_tags] += 1
+         
          prev_tag = tag
 
-   return tag_bigram, tag_counts, word_tag_counts
+   return tag_bigram, tag_counts, word_tag_counts, suff_tag_counts
 
 def build_transition_matrix(tag_bigram, tag_counts): 
-   global TAGS, T_c
+   global TAGS, T_c, T_MAT
+
    n_tags = len(TAGS)
-   t_mat = np.zeros((n_tags, n_tags-1), dtype=np.float64) # prev tag by cur tag 
+   T_MAT = np.zeros((n_tags, n_tags-1), dtype=np.float64)
 
    for i, tag1 in enumerate(TAGS): 
       for j, tag2 in enumerate(T_c): 
-
          bi_count = tag_bigram[(tag1, tag2)] if (tag1, tag2) in tag_bigram.keys() else 0
-         t_mat[i, j] = (bi_count + 1)/ (len(tag_counts[tag1]) + n_tags) # add one smoothing 
+         T_MAT[i, j] = (bi_count + 1) / (len(tag_counts[tag1]) + n_tags) # add one smoothing 
          # denom: add tag_counts[tag1] + num_unique bigrams for tag1
    
-   return t_mat 
-
 def build_emission_matrix(tag_counts, word_tag_counts): 
-   global T_c, CORPUS
+   global T_c, CORPUS, E_MAT
 
    n_tags = len(T_c)
    n_words = len(CORPUS)
 
-   emission_mat = np.zeros((n_words, n_tags), dtype=np.float64)
+   E_MAT = np.zeros((n_words, n_tags), dtype=np.float64)
    
-   for i, tag in enumerate(T_c): 
-      for j, word in enumerate(CORPUS): 
+   for i, word in enumerate(CORPUS): 
+      for j, tag in enumerate(T_c): 
          t_count = word_tag_counts[word][tag] if tag in word_tag_counts[word].keys() else 0
-         emission_mat[j, i] = t_count / len(tag_counts[tag])
-
-   return emission_mat   
-
-def train_model():
-   global CORPUS, TAGS, N_UNIQ_TAG, E_MAT, T_MAT, T_c
+         E_MAT[i, j] = t_count / len(tag_counts[tag])
    
-   train_set = word_tuples(read_file())
+def train_model():
+   global CORPUS, TAGS, N_UNIQ_TAG, T_c, SUFF_TAG_COUNTS
 
-   tag_bigram, tag_counts, word_tag_counts = get_tag_counts(train_set)
-
+   tag_bigram, tag_counts, word_tag_counts, SUFF_TAG_COUNTS = get_tag_counts()
+   
    CORPUS = list(set(word_tag_counts.keys()))
    N_UNIQ_TAG = {k:len(set(v)) for k,v in tag_counts.items()}
    TAGS = list(set(N_UNIQ_TAG.keys()))
    T_c = TAGS.copy()
    T_c.remove('')
 
-   T_MAT = build_transition_matrix(tag_bigram, tag_counts)
-   E_MAT = build_emission_matrix(tag_counts, word_tag_counts)
+   build_transition_matrix(tag_bigram, tag_counts)
+   build_emission_matrix(tag_counts, word_tag_counts)
 
 def save_model(): 
-   global CORPUS, N_UNIQ_TAG, E_MAT, T_MAT, T_c
+   global CORPUS, TAGS, N_UNIQ_TAG, SUFF_TAG_COUNTS, E_MAT, T_MAT 
 
-   vocab=" ".join(CORPUS)
+   vocab = " ".join(CORPUS)
+
    np.set_printoptions(threshold=sys.maxsize)
    e_mat_str = np.array2string(E_MAT)
    t_mat_str = np.array2string(T_MAT)
 
    with open(OUT_PATH, "w") as f: 
       f.write(f"corpus:\n\n{vocab}\n\n")
+      f.write(f"possible tags given suffix:\n\n{json.dumps(SUFF_TAG_COUNTS)}\n\n")
+
       f.write(f"tags:\n\n{json.dumps(N_UNIQ_TAG)}\n\n")
       f.write(f"matrix tag order:\n\n{json.dumps(TAGS)}\n\n")
+
       f.write(f"Emission Matrix (dimensions: words x tags)\n\n{e_mat_str}\n\n")
-      f.write(f"Transition Matrix (dimensions: tags+1 x tags)\n\n{t_mat_str}")
+      f.write(f"Transition Matrix (dimensions: tags+1 x tags)\n\n{t_mat_str}\n\n")
 
 def main(): 
 
